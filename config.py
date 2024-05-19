88da09c297d319fa61da169c486b80dab851ff72
Abelboby <136153140+Abelboby@users.noreply.github.com>
2024-05-20 01:29:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ import os
 class Config(object):
 
     # Get a bot token from botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7132282392:AAHWSjExwrcyshy7WgYXqYJOYvv-yMJLFIU
+")
 
     # Get from my.telegram.org (or @UseTGXBot)
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", "29761366"))
+    API_HASH = os.environ.get("API_HASH", "f895f8fd4d7fd7fc98b3d4108f6f2c66")
