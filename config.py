d0f88254c2bcd920da3789c3ea99f4f3ba5bf8f2
Abelboby <136153140+Abelboby@users.noreply.github.com>
2024-05-20 01:39:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-# #!/usr/bin/env python3
-# # -*- coding: utf-8 -*-
-# # @trojanzhex
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @trojanzhex
 
-# import os
+import os
 
 
-# class Config(object):
+class Config(object):
 
-#     # Get a bot token from botfather
-#     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7132282392:AAHWSjExwrcyshy7WgYXqYJOYvv-yMJLFIU
-# ")
+    # Get a bot token from botfather
+    BOT_TOKEN ="7132282392:AAHWSjExwrcyshy7WgYXqYJOYvv-yMJLFIU"
 
-#     # Get from my.telegram.org (or @UseTGXBot)
-#     APP_ID = int(os.environ.get("APP_ID", "29761366"))
-#     API_HASH = os.environ.get("API_HASH", "f895f8fd4d7fd7fc98b3d4108f6f2c66")
+    # Get from my.telegram.org (or @UseTGXBot)
+    APP_ID = "29761366"
+    API_HASH = "f895f8fd4d7fd7fc98b3d4108f6f2c66"
